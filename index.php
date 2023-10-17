9af2387f7755a4d5c32fe29e311eda36c69939ab
Gerson Borja <gerson.borja2018@gmail.com>
2023-10-17 15:13:28 -0600

    se agrego el saber cuantos clics genera un usuario

--- index.php
+++ index.php
@@ -92,7 +92,7 @@ $app->group('/auth', function ($group) {
 $app->group('/view', function ($group) {
 
     $group->post('/logger', CounterController::class . ':view');
-    $group->post('/clics', CounterController::class . ':clics')->add($validateJwtMiddleware);
+    $group->post('/clics', CounterController::class . ':clics');
 
 });
 $app->run();
